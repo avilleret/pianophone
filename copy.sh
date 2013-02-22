a851116c3c8fb1977cf30db9cc5da2e790a56d0b
Antoine Villeret <antoine.villeret@gmail.com>
2013-02-22 18:25:30 +0000

    improve copy.sh;;;

--- copy.sh
+++ copy.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-if [ -d /media/usb/pianophone-patches ]; then
-	echo Copying files...
-	if [ -d /home/pi/pianophone/patches ]; then
-		mv /home/pi/pianophone/patches /home/pi/pianophone/patches_bak
+date > ~/log
+
+for drive in  /dev/sd??* 
+do
+	echo mount $drive....
+	mount $drive /media/usb
+	if [ -d /media/usb/pianophone-patches ]; then
+		echo Copying files...
+		if [ -d /home/pi/pianophone/patches ]; then
+			rm -rf /home/pi/pianophone/patches
+		fi
+		cp -r /media/usb/pianophone-patches /home/pi/pianophone/patches
+		echo OK
+	else
+		echo Can\'t find \"pianophone-patches\" folder on usb drive
 	fi
-	cp -r /media/usb/pianophone-patches /home/pi/pianophone/patches
+	echo -n "umount..."
+	umount /media/usb
 	echo OK
-else
-	echo Can\'t find \"pianophone-patches\" folder on usb drive
-fi
+done
+
+# umount /media/usb
 
 exit
