cac915c11314c43a23aafb12d227224ed9b82673
Antoine Villeret <antoine.villeret@gmail.com>
2013-05-04 19:18:34 +0000

    select ESI sound card on startup

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,6 +8,6 @@ else
 	echo start pianophone...
 	cd /home/pi/pianophone
 	./pianophone &
-	su pi -c "pd -nogui /home/pi/pianophone/patches/abs/loader.pd" &
+	su pi -c "pd -rt -alsa -noadc -audiooutdev 3 -blocksize 64 -r 48000 -audiobuf 10 -nogui /home/pi/pianophone/patches/abs/loader.pd" &
 fi
 
