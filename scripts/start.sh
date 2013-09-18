ba64a2e2016351b3cfdd93c1aaf5c5fbe737db27
Antoine Villeret <antoine.villeret@gmail.com>
2013-09-18 22:10:17 +0000

    fix boot sequence

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,8 +7,11 @@ if [ $PID ] ; then
 else
 	echo start pianophone...
 	cd /home/pi/pianophone
+#        /home/pi/pianophone/scripts/disable_ethernet.sh
+#        /home/pi/pianophone/lcd "Ethernet disable" 
+#        sleep 1
 	/home/pi/pianophone/lcd "Start puredata"
-	su pi -c "pd -rt -alsa -noadc -audiooutdev 3 -blocksize 64 -r 48000 -audiobuf 10 -nogui /home/pi/pianophone/patches/abs/loader.pd" &
+	su pi -c "pd -nogui -rt -alsa -audiodev 3 -blocksize 64 -r 48000 -audiobuf 10 /home/pi/pianophone/patches/abs/loader.pd" &
         sleep 1
         /home/pi/pianophone/lcd "Start Pianophone"
         ./pianophone &
