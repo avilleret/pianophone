2a2a91aa52d7ac58b7d06981f071eecea8963816
Antoine Villeret <antoine.villeret@gmail.com>
2013-09-19 00:35:10 +0000

    simplification

--- scripts/disable_ethernet.sh
+++ scripts/disable_ethernet.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-/home/pi/pianophone/lcd "Ethernet disable"
 _ETH0=$(ifconfig | grep eth0) || true
 
 if [ "$_ETH0" ]; then
 	dhclient -r
+   /etc/init.d/networking stop
    echo -n "1-1.1:1.0" | sudo tee /sys/bus/usb/drivers/smsc95xx/unbind
+   /home/pi/pianophone/lcd "Ethernet disable"
 fi
 
--- scripts/enable_ethernet.sh
+++ scripts/enable_ethernet.sh
@@ -6,7 +6,7 @@ if [ "$_ETH0" ]; then
 	echo eth0 found
 else
 	echo -n "1-1.1:1.0" | sudo tee /sys/bus/usb/drivers/smsc95xx/bind
+   /etc/init.d/networking start
    dhclient eth0
+   /home/pi/pianophone/lcd "Ethernet enable"
 fi
-
-
