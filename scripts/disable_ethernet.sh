ae105586309366ab388994ccf91d65715804c5b8
Antoine Villeret <antoine.villeret@gmail.com>
2013-10-04 13:12:37 +0000

    don't disable networking service to use OSC maybe OSC on localhost doesn't need networking service...

--- scripts/disable_ethernet.sh
+++ scripts/disable_ethernet.sh
@@ -2,8 +2,7 @@
 _ETH0=$(ifconfig | grep eth0) || true
 
 if [ "$_ETH0" ]; then
-	dhclient -r
-   /etc/init.d/networking stop
+   dhclient -r
    echo -n "1-1.1:1.0" | sudo tee /sys/bus/usb/drivers/smsc95xx/unbind
    /home/pi/pianophone/lcd "Ethernet disable"
 fi
